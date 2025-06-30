ea428be55f6b3e8dfdaeafc877e25b1411050332
juliafrasquetti <juliafrasquetti10@gmail.com>
2025-06-30 12:54:06 +0000

    de

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-const numeroSenha = document.querySelector('.parametro-senha__texto';)
+const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelectorA11 ('.parametro-senha__botao');
+const botoes = document.querySelectorAll ('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho () {
-    if (tamanhoSenha > 1) {
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
         tamanhoSenha --;
-}
-numeroSenha.textContent = tamanhoSenha;
+    }
+    numeroSenha.textContent = tamanhoSenha;
 }
 
-function aumentaTamanho (){ 
-    if(tamanhoSenha < 20){
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
         tamanhoSenha ++;
-}
-numeroSenha.textContent = tamanhoSenha;
+    }
+    numeroSenha.textContent = tamanhoSenha;
 }
